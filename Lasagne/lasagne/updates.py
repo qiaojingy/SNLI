fef08a58df971b2537122f14c9260ed832014c24
qiaojingy <qiaojingyan9@gmail.com>
2016-03-04 08:57:45 -0800

    modified updates.py

--- Lasagne/lasagne/updates.py
+++ Lasagne/lasagne/updates.py
@@ -598,7 +598,8 @@ def adam(loss_or_grads, params, learning_rate=0.001, beta1=0.9,
         if masks != None:
           for (paramName, mask) in masks:
             if param.name == paramName:
-              updates[param] = updates[param] * mask
+              print("{} is masked".format(param.name))
+              updates[param] = param - mask * step
 
     updates[t_prev] = t
     return updates
